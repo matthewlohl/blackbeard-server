06af753078a792257084996f497288ab68610d6c
Matthew Lo <matthewlol316@gmail.com>
2022-10-28 18:59:15 +0100

    connect auth to client

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 from flask import Flask, jsonify, render_template, request, session
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
-
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, rooms
+from flask_marshmallow import Marshmallow
 
 from config import ApplicationConfig
 
 from models import db, users
 
 
-
 app = Flask(__name__)
 
 app.config.from_object(ApplicationConfig)
 
+# --- using dependencies in app ---
+
 bcrypt = Bcrypt(app)
+ma = Marshmallow(app)
+cors = CORS(app, resource={
+    r"/*": {
+        "origins": "*"
+    }
+}, supports_credentials=True)
+
+# --- Define your output format with marshmallow. --- 
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username")
+
+users_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+socketio = SocketIO(app, cors_allowed_origins = '*')
+
+# --- create database ---
 
 db.init_app(app)
-CORS(app, supports_credentials=True)
-
 with app.app_context():
     db.create_all()
 
-@app.route('/register', methods=['POST', 'GET'])
+# --- ROUTES --- 
+
+@app.route('/register', methods=['POST'])
 def register():
     username = request.json['username']
     password = request.json['password']
@@ -38,7 +58,7 @@ def register():
             "username": new_user.username
         })
 
-
+@cross_origin
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     username = request.json["username"]
@@ -62,7 +82,7 @@ def login():
 
     response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return response
+    return response, 201
 
 @app.route('/user')
 def get_current_user():
@@ -74,6 +94,25 @@ def get_current_user():
         "id": user.id,
         "username": user.username
     })
+
+# --- SOCKETS --- 
+
+players = []
+
+# @socketio.on('connect')
+# def connection(socket):
+#     print('A new player just connected on'+socket.id)
+
+# @socketio.on('create')
+# def createRoom(data):
+#     print("Room ID "+ data['roomID'] + " has been created")
+#     print(data['creator'] + " has created the room")
+#     join_room(data['roomID'])
+#     # roomData = {'roomID': data['roomID'], 'username': [data['username']]}
+#     players.append(data['creator'])
+#     print(players)
+#     send(data['roomID'] + ' has been created', to=data['roomID'])
+#     send(players, broadcast = True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class ApplicationConfig:
     SECRET_KEY = "be6865bf45c2fdf0a1997cc0f4e7c488"
     SQLALCHEMY_DATABASE_URI = r'sqlite:///db.sqlite3'
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
