af65febaa95f77f61fd9f06a51a5f5384225d1a7
Babole <stefancmircea@gmail.com>
2022-11-01 00:46:37 +0000

    fix deployment setup 5

--- game/__init__.py
+++ game/__init__.py
@@ -41,6 +41,6 @@ db.init_app(app)
 # app.register_blueprint(main_routes)
 
 # Main
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5001))
+#     app.run(host='0.0.0.0', port=port)
